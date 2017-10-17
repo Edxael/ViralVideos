513d9f2399b00c435b167cf8b62ec33ab429f2d5
Edxael <zerphyr.r@gmail.com>
2017-10-16 23:04:53 -0600

    1st Comm.

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var ejs = require('ejs');
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/views'));
+app.use(morgan('dev'));
+
+
+app.get('/', function(req, res){
+  res.render('pages/00-index.html');
+});
+
+// app.get('/ah', function(req, res){
+//   res.render('pages/01-ayu.ejs');
+// });
+//
+// app.get('/mc', function(req, res){
+//   res.render('pages/02-madc.ejs');
+// });
+//
+// app.get('/bp', function(req, res){
+//   res.render('pages/03-bpink.ejs');
+// });
+//
+// app.get('/ty', function(req, res){
+//   res.render('pages/04-taeyang.ejs');
+// });
+
+
+
+
+app.listen((process.env.PORT || 5000), function(err){
+  if(err){ throw err; }
+  console.log("Server Up & Running ......");
+});
